501aa7e82926320fcfc8e4e202f4a23f6144adef
seonmo <mo3509@gmail.com>
2018-07-25 19:28:30 +0900

    [2018-07-25] message 관련추가

--- src/main/java/com/bluedigm/demo/api/sample/controller/SampleRestController.java
+++ src/main/java/com/bluedigm/demo/api/sample/controller/SampleRestController.java
@@ -2,6 +2,8 @@ package com.bluedigm.demo.api.sample.controller;
 
 import com.bluedigm.demo.api.sample.model.SampleModel;
 import com.bluedigm.demo.api.sample.service.SampleService;
+import com.bluedigm.demo.common.message.MessageCode;
+import com.bluedigm.demo.common.message.Messages;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +20,24 @@ public class SampleRestController {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
+	private Messages messages;
+
+	@Autowired
 	private SampleService sampleService;
 
 	@GetMapping
-	public List<SampleModel> selectSample() throws Exception{
+	public List<SampleModel> selectSample(){
 		logger.info("SampleRestController - selectSample");
 		return sampleService.selectSample();
+	}
+
+	@GetMapping(value = "/messageTest")
+	public String messageTest() {
+		logger.info("SampleRestController - messageTest");
+		StringBuffer sbMessage = new StringBuffer();
+		sbMessage.append(messages.getMessage(MessageCode.RESOURCE_NOT_FOUND, "Error Message Test!!"));
+		sbMessage.append("<br>");
+		sbMessage.append(messages.getMessage(MessageCode.MESSAGE_TEST, "Message Test!!"));
+		return sbMessage.toString();
 	}
 }
---
+++ src/main/java/com/bluedigm/demo/common/message/Code.java
@@ -0,0 +1,14 @@
+package com.bluedigm.demo.common.message;
+
+/**
+ * The interface Code.
+ */
+public interface Code {
+
+    /**
+     * Gets code.
+     *
+     * @return the code
+     */
+    public String getCode();
+}
